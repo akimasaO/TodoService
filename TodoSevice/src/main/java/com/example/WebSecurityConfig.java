7937fa67d9a9dd2fd5dc3dd9b30ba29bd69526a3
akimasaO <exa.oohara@gmail.com>
2020-01-17 17:50:41 +0900

    spring securityを導入すると全てのリクエストに認証が必要になる？

--- TodoSevice/src/main/java/com/example/WebSecurityConfig.java
+++ TodoSevice/src/main/java/com/example/WebSecurityConfig.java
@@ -12,6 +12,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //アプリケーションにアクセスするときはログインページを通ることになる
+        //ログインページへのアクセスは全員許可する。
+        http.formLogin()
+            .loginPage("/delete")
+            .permitAll();
+        
+//        //会員登録機能へのアクセスは全て許可する
+//        http.authorizeRequests()
+//            .antMatchers("/register").permitAll()
+//            .anyRequest().authenticated();
+
+        // 全てのリクエストに対して認証をかける -> 認証されない場合ステータス403が返却される
+        http.authorizeRequests()
+            .anyRequest().authenticated();
+    }
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
